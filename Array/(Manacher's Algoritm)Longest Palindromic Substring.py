e9d12a2cf6743d51441708f7a2c44dfeb740a086
Aditya <agauti003@gmail.com>
2019-09-28 15:09:04 +0530

    initial commit

---
+++ Array/(Manacher's Algoritm)Longest Palindromic Substring.py
@@ -0,0 +1,40 @@
+# Hi, here's your problem today. This problem was asked by Twitter:
+
+# A palindrome is a sequence of characters that reads the same backwards and forwards.
+# Given a string, s, find the longest palindromic substring in s.
+
+# Example:
+# Input: "banana"
+# Output: "anana"
+
+# Input: "million"
+# Output: "illi"
+# class Solution:
+#     def longestPalindrome(self, s):
+#       # Fill this in.
+
+# # Test program
+# s = "tracecars"
+# print(str(Solution().longestPalindrome(s)))
+# # racecar
+
+
+class Solution:
+    def checkPalindrome(str):
+        # reversing a string in python to get the reversed string
+        # This is extended slice syntax.
+        # It works by doing [begin:end:step] - by leaving begin and end off and specifying a step of -1,
+        # it reverses a string.
+        reversedString = str[::-1]
+        if (str == reversedString):
+            return true
+        else:
+            return false
+
+    def longestPalindrome(str):
+        while (index > str.length):
+            while ():
+                pass
+
+            
+            
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Data-Structure
+
 Data Structure Interview Question With Answers
+
+> Click :star:if you like the project. Pull Request are highly appreciated.
+
+### Table of Contents
+
+| No. | Questions                                          |
+| --- | -------------------------------------------------- |
+|     | **Array**                                          |
+| 1   | [Manachers Algoritm Longest Palindrome Substring?] |
